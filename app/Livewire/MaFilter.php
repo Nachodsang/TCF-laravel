eb7fc3b58cd13aad927f01fa9b929ac334ec6869
rachataa <rachata.arnankul@gmail.com>
2024-02-27 15:08:15 +0700

    15.08

--- app/Livewire/MaFilter.php
+++ app/Livewire/MaFilter.php
@@ -3,25 +3,14 @@
 namespace App\Livewire;
 
 use App\Models\MaIndustryMd;
-use App\Models\MaProductMd;
 use Livewire\Component;
-use Livewire\WithPagination;
 
 class MaFilter extends Component
 {
-    use WithPagination;
-
     public function updateIndustry($sortId)
     {
         foreach ($sortId as $id) {
             MaIndustryMd::where('id', $id['value'])->update(['sort' => $id['order']]);
-        }
-    }
-
-    public function updateProduct($sortId)
-    {
-        foreach ($sortId as $id) {
-            MaProductMd::where('id', $id['value'])->update(['sort' => $id['order']]);
         }
     }
 
